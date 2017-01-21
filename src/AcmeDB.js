184c7d10f1c22937cbe3b86d7e0e710045edcb47
Eric Katz <ericpkatz@gmail.com>
2017-01-21 10:22:21 -0500

    Profs comments...
    Good work

--- src/AcmeDB.js
+++ src/AcmeDB.js
@@ -1,5 +1,6 @@
 class AcmeDB {
   constructor({ users }){
+    //dry this out by using the addUser function
     for (var i = 0; i < users.length; i++){
       this.users = [{name: users[i].name, id: i + 1}];
     }
@@ -7,6 +8,7 @@ class AcmeDB {
 
   addUser(user){
    var max = 0;
+   //good logic.. but use reduce
    this.users.forEach(function(user){
       if (max < user.id){
         max = user.id;
@@ -18,6 +20,7 @@ class AcmeDB {
   }
 
   showUsers (){
+    //use the map function to pull out the names, then join
     var str = "";
     this.users.forEach(function(item){
       str += item.name + " ";
@@ -26,7 +29,9 @@ class AcmeDB {
   }
 
   findById (id){
-    var person = "";
+    //if you want to exit early out of foreEach.. then maybe just use basic for loop
+    //filter could also work here as well.
+    var person = "";//i don't think you have to assign anything here. also use let
     this.users.forEach(function(item){
       if (item.id === id){
         person = item;
@@ -36,6 +41,7 @@ class AcmeDB {
   }
 
   removeUserById (id){
+    //might just be easier to use filter
     var index = 0;
     for (var i = 0; i < this.users.length; i++){
       if (this.users[i].id === id){
@@ -47,6 +53,7 @@ class AcmeDB {
   }
 
   editUser(user){
+    //map works well here.. just replace the user with the passed in user if the id's match...
     var index = 0;
     for (var i = 0; i < this.users.length; i++){
       if (this.users[i].id === user.id){
@@ -54,7 +61,7 @@ class AcmeDB {
         break;
       }
     }
-    this.users[index].name = user.name;
+    this.users[index].name = user.name;//what if there were other properties which were being modified here..
   }
 }
 
